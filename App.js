8ca129d0d0d9be2490c35419b3e7d23b2b75af5a
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-30 06:45:44 +0300

    Create a path for the homepage because there will be another path when clicking the author

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import Quotes from './components/Quote';
-import RandomQuotes from './components/RandomQuotes';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import QuotesRandomly from './components/QuotesRandomly';
 
 export default function App() {
-    const [quotes, setQuotes] = useState([]);
-
-    const fetchGoutes = async () => {
-        try{
-            const res = await fetch("https://quote-garden.herokuapp.com/api/v2/quotes/random");
-            const randomQuotes = await res.json();
-            setQuotes(randomQuotes.quote);
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    useEffect(() => {
-        fetchGoutes();
-
-    }, []);
-    console.log(quotes);
-    
     return (
-        <section>
-            <Quotes quotes={quotes} />
-            <RandomQuotes setQuotes={setQuotes} quotes={quotes} />
-        </section>
+        <div>
+			<Router>
+				<Switch>
+					<Route path='/'>
+						<QuotesRandomly />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
     )
 }
