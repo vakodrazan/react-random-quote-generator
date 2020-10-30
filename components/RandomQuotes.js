c14c476f8095b8c5ea1f97f15f2a3ba6f3c6651a
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-10-30 06:44:48 +0300

    Trying to work on the new random quotes

--- components/RandomQuotes.js
+++ components/RandomQuotes.js
@@ -4,9 +4,9 @@ export default function RandomQuotes({quotes, setQuotes}) {
 
     const handleClick = () => {
         console.log(quotes);
-        // const randomNumber = Math.floor(Math.random() * quotes.length);
-        // setQuotes(randomNumber)
+        setQuotes(quotes)
     }
+
     return (
         <button onClick={handleClick}>Random</button>
     )
